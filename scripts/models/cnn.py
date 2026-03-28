fixed issue with incorrect dims

scripts/models/cnn.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self, out_channel=256):
+    def __init__(self, input_channel = 6, out_channel=256):
         super().__init__()
         
         self.features = nn.Sequential(
             #Layer collection 1
-            nn.Conv2d(6, 32, kernel_size=3, padding=1),
+            nn.Conv2d(input_channel, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),

scripts/models/full_model.py
@@ -11,14 +11,15 @@ class FQBNeck(nn.Module):
         super().__init__()
         
         self.fft = FFT()
-        self.cnn = CNN(feature_dim)
+        self.rgb_cnn = CNN(input_channel=3, out_channel=feature_dim)
+        self.fft_cnn = CNN(input_channel=6, out_channel=feature_dim)
         self.vib = VIB(feature_dim, latent_dim)
         self.mlp = MLP(latent_dim, num_class)
         
     def forward(self, x):
         #x = self.fft(x)
-        rgb_features = self.cnn(x)
-        fft_features = self.cnn(self.fft(x))
+        rgb_features = self.rgb_cnn(x)
+        fft_features = self.fft_cnn(self.fft(x))
         
         features = torch.cat([fft_features, rgb_features], dim=1)
         