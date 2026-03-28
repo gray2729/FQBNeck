e7d9b14865a16d41ba3c127f4b83052ea357d298
isaac gray <igray2729@gmail.com>
2026-03-28 11:34:27 -0400

    removed testing printouts

--- scripts/utils/loss_function.py
+++ scripts/utils/loss_function.py
@@ -8,6 +8,4 @@ def VIB_loss(logits, target, mu, logvar, beta=0.001):
     kl_div = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp())
     kl_div = kl_div.mean()
     
-    print("CE:", ce_loss.item(), "KL:", kl_div.item())
-    
     return ce_loss + beta * kl_div
