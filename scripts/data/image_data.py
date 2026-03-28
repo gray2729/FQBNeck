774098cd0d367a7aca681d1a2899ac7be50cbac8
isaac gray <igray2729@gmail.com>
2026-03-28 12:59:41 -0400

    reversed to cnn instead of resnet

--- scripts/data/image_data.py
+++ scripts/data/image_data.py
@@ -13,11 +13,7 @@ class ImageData(Dataset):
 
         self.transform = transforms.Compose([
             transforms.Resize((image_size, image_size)),
-            transforms.ConvertImageDtype(torch.float32),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
+            transforms.ConvertImageDtype(torch.float32)
         ])
 
     def load_paths(self):
