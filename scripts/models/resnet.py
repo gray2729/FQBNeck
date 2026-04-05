511eb2a998dea792e0bc43de842aeb3b69af97d3
isaac gray <igray2729@gmail.com>
2026-04-05 12:01:09 -0400

    fixed resnet

--- scripts/models/resnet.py
+++ scripts/models/resnet.py
@@ -6,7 +6,7 @@ class ResNet(nn.Module):
     def __init__(self, output_dim=128, pretrained=True):
         super().__init__()
         
-        resnet = models.resnet18(weights=models.ResNet50_Weights.DEFAULT if pretrained else None)
+        resnet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT if pretrained else None)
         
         self.layers = nn.Sequential(*list(resnet.children())[:-1])
         self.project = nn.Linear(2048, output_dim)
